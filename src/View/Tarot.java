a465484975e17cd06d8b020cdfbec7cf88a88af6
Roman Rieunier <rieunierroman@hotmail.fr>
2016-11-24 17:52:46 +0100

    Initial release

---
+++ src/View/Tarot.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.util.Observable;
+import java.util.Observer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Roman
+ */
+public class Tarot extends Application implements Observer {
+   
+   @Override
+   public void start(Stage primaryStage) {
+      Button btn = new Button();
+      btn.setText("Say 'Hello World'");
+      btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	 @Override
+	 public void handle(ActionEvent event) {
+	    System.out.println("Hello World!");
+	 }
+      });
+      
+      StackPane root = new StackPane();
+      root.getChildren().add(btn);
+      
+      Scene scene = new Scene(root, 300, 250);
+      
+      primaryStage.setTitle("Hello World!");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+   }
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      launch(args);
+   }
+
+   @Override
+   public void update(Observable o, Object arg) {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
+   
+}
